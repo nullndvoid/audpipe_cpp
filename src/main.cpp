b113871cdf1165dcfebd3db6fca807aa9fc980f0
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-03-11 20:34:09 +0000

    Fix missing cryptopp, add spdlog, boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
-#include <uvgrtp/lib.hh>
+#include <spdlog/spdlog.h>
 
-int main() { return 0; }
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+#include <uvgrtp/lib.hh>
+#include <uvgrtp/util.hh>
+
+constexpr uint16_t REMOTE_PORT = 8890;
+constexpr uint16_t LOCAL_PORT = 8891;
+
+int main() {
+  auto stderr_log = spdlog::stderr_color_mt("audpipe");
+
+  uvgrtp::context ctx;
+  uvgrtp::session *sess = ctx.create_session("127.0.0.1");
+
+  if (sess == nullptr) {
+    stderr_log->error("Failed to create uvgRTP session. Must be OOM.");
+    return 1;
+  }
+
+  int flags = RCE_SEND_ONLY;
+  uvgrtp::media_stream *opus_stream =
+      sess->create_stream(LOCAL_PORT, REMOTE_PORT, RTP_FORMAT_OPUS, flags);
+
+  if (opus_stream == nullptr) {
+    stderr_log->error("Failed to create opus stream.");
+    return 1;
+  }
+
+  // We want to get some audio input. For now, accept microphone.
+}
