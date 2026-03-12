1affa3dc09f5a7e7b80805fed6150befdcb22f0d
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-03-12 17:38:08 +0000

    Automatically choose first monitor found
    TODO: Check what device is set as default, handle things like devices being unplugged/disconnected gracefully.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
@@ -16,15 +17,20 @@ int main() {
   auto local_address = std::string("127.0.0.1");
   auto rtp = Rtp(local_address, LOCAL_PORT, REMOTE_PORT, RTP_SEND);
 
-  // We want to get some audio input. For now, accept microphone.
   auto &audio = AudioBackend::instance();
   auto inputs = audio.get_inputs();
-  auto outputs = audio.get_outputs();
 
   audio.set_data_callback([&](const uint8_t *data, size_t len) {
     // TODO: Encode to Opus and send via RTP.
     stderr_log->debug("Received {} bytes of audio data.", len);
   });
 
-  audio.record(inputs.front());
+  // Find first monitor input.
+  auto monitor = std::ranges::find_if(
+      inputs, [&](AudioDevice &dev) { return dev.is_monitor; });
+
+  stderr_log->info("First monitor device found is \'{}\'.",
+                   monitor->description);
+
+  audio.record(*monitor.base());
 }
