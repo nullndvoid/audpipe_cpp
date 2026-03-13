c329aa84c07b290d5b22b0d48b767e4f6d4e9a4f
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-03-13 03:25:31 +0000

    Hide debug logging by default

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ constexpr uint16_t LOCAL_PORT = 8891;
 
 int main() {
   auto stderr_log = spdlog::stderr_color_mt("audpipe");
-  stderr_log->set_level(spdlog::level::debug);
+  stderr_log->set_level(spdlog::level::info);
 
   Server serv = Server(std::string("127.0.0.1"), LOCAL_PORT, REMOTE_PORT);
 }
