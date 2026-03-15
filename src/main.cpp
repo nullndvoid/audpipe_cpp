6fd17fedcde5fbd4b15d4040338125489211bb41
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-03-15 01:49:50 +0000

    Add print-config, subcommands

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "client.hxx"
 #include "server.hxx"
 
 #include <cstdint>
@@ -23,6 +24,7 @@ int main(int argc, char **argv) {
   std::string local_ip;
   uint16_t local_port;
   uint16_t remote_port;
+  bool print_config{false};
 
   auto home_dir = std::getenv("HOME");
   if (home_dir == nullptr) {
@@ -40,11 +42,15 @@ int main(int argc, char **argv) {
 
   app.get_formatter()->column_width(40);
   app.get_formatter()->enable_option_type_names(false);
+  // Ignore extra fields, these could later be parsed as TOML if required.
+  app.allow_config_extras(CLI::config_extras_mode::ignore);
 
   auto *client = app.add_subcommand(
       "client", "Client (remote audio stream to virtual input)");
   auto *server =
       app.add_subcommand("server", "Server (forwards input to remote client)");
+  client->configurable(true);
+  server->configurable(true);
 
   auto add_common_opts = [&](CLI::App *sub) {
     sub->add_option("-i,--local-ip", local_ip,
@@ -57,6 +63,11 @@ int main(int argc, char **argv) {
     sub->add_option("-p,--remote-port", remote_port, "Remote port for RDP.")
         ->check(CLI::Range(1024, 65535))
         ->default_val(DEFAULT_REMOTE_PORT);
+
+    auto *print_config_flag =
+        sub->add_flag("--print-config", print_config,
+                      "Print effective config and exit (TOML).");
+    print_config_flag->configurable(false);
   };
 
   add_common_opts(client);
@@ -65,4 +76,26 @@ int main(int argc, char **argv) {
   app.require_subcommand(1);
 
   CLI11_PARSE(app, argc, argv);
+
+  auto print_section_config = [&](CLI::App *sub, bool desc) {
+    std::cout << "[" + sub->get_name() + "]\n"
+              << sub->config_to_str(true, desc);
+  };
+
+  auto subs = app.get_subcommands();
+
+  if (print_config) {
+    for (auto sub : subs) {
+      if (sub->parsed())
+        print_section_config(sub, false);
+    }
+
+    return 0;
+  }
+
+  if (app.got_subcommand(server)) {
+    Server(local_ip, local_port, remote_port);
+  } else if (app.got_subcommand(client)) {
+    Client(local_ip, local_port, remote_port);
+  }
 }
