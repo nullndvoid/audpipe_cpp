fa3c8776cf90f97b8d43745e24a844e1e644aac9
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-03-15 02:19:23 +0000

    Hide CLI ugliness from main

---
+++ include/config.hxx
@@ -0,0 +1,10 @@
+// Defines we might want to set application-wide. Currently most of these are
+// just consumed in `parse_cli`.
+#ifndef __AUDPIPE_CONFIG
+#define __AUDPIPE_CONFIG
+
+#define DEFAULT_REMOTE_PORT 8890
+#define DEFAULT_LOCAL_PORT 8891
+#define DEFAULT_LOCAL_IP "0.0.0.0"
+
+#endif
