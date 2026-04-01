c5543fcccb16ca268e28aeac911503879b8d762a
J. Hinchliffe <jjh413@student.bham.ac.uk>
2026-04-01 23:38:11 +0100

    Remove old CLI code, simplify usage
    TODO: Allow custom config file locations.

--- include/cli.hxx
+++
@@ -1,9 +0,0 @@
-#ifndef __AUDPIPE_CLI
-#define __AUDPIPE_CLI
-
-// Parse the CLI arguments. This is mostly just to keep main very empty, like I
-// prefer. Plus the CLI code is a tad ugly, and we might want some other ways to
-// interact with the application down the line(?)
-int parse_cli(int argc, char **argv);
-
-#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,44 @@
+#include "config.hxx"
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
-#include "cli.hxx"
+inline void print_usage() {
+  std::cerr << "Usage: audpipe (client | server)" << std::endl;
+}
 
 int main(int argc, char **argv) {
   auto stderr_logger = spdlog::stderr_color_mt("audpipe");
   stderr_logger->set_level(spdlog::level::info);
-  return parse_cli(argc, argv);
+
+  auto die = [&] {
+    print_usage();
+    exit(1);
+  };
+
+  if (argc < 2)
+    die();
+
+  auto subcommand = argv[1];
+  Mode mode = {};
+
+  if (strcmp(subcommand, "client") == 0) {
+    mode = Mode::CLIENT;
+  } else if (strcmp(subcommand, "server") == 0) {
+    mode = Mode::SERVER;
+  } else {
+    die();
+  }
+
+  try {
+    auto cfg = Config("./audpipe.toml", mode);
+
+    stderr_logger->info("Local: {}:{}", cfg.local_ip, cfg.local_port);
+    stderr_logger->info("Remote: {}:{}", cfg.remote_ip, cfg.remote_port);
+  } catch (...) {
+    // Presume it was logged upstream and just quit.
+    exit(1);
+  }
 }
